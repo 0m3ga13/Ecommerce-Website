cf92a5ef88bbd2d0cc6c981632c35e98e35aa0fb
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-03-04 02:04:29 +0100

    Update about.vue

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative h-screen isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <div class="relative h-screen isolate sm:overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img src="https://images.unsplash.com/photo-1536300099515-6c61b290b654?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" />
       <svg viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
         <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
