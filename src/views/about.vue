882e03f5f8c9763f5c9f45c08ccb9266d405a3d4
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-03-04 02:15:22 +0100

    Update about.vue

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sm:relative sm:h-screen sm:isolate sm:overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <div class="relative h-screen isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img src="https://images.unsplash.com/photo-1536300099515-6c61b290b654?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" />
       <svg viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
         <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
@@ -24,11 +24,11 @@
           <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Why choosing us?</h2>
           <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
         </div>
-        <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+        <div class=" mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
           <div class="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
             <a v-for="link in links" :key="link.name" :href="link.href">{{ link.name }} <span aria-hidden="true">&rarr;</span></a>
           </div>
-          <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+          <dl class="hidden mt-16 sm:grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
             <div v-for="stat in stats" :key="stat.name" class="flex flex-col-reverse">
               <dt class="text-base leading-7 text-gray-300">{{ stat.name }}</dt>
               <dd class="text-2xl font-bold leading-9 tracking-tight text-white">{{ stat.value }}</dd>
