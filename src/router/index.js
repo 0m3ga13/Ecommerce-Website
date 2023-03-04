6d4ad1a1c533d0dd1415ea58644e5e8699fafb2e
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-03-04 02:51:21 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Item from '../views/Item.vue';
 import About from '../views/about.vue'
 import HomePage from '../views/homePage.vue'
 import Contact from '../views/contact.vue'
+import error from '../views/404.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
