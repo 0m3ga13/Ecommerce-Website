cd1945b0313525601286fab83b6b90475db4dd46
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-03-05 00:27:33 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './assets/normalize.css'
 import './assets/main.css'
-import 'es6-promise/auto'
-import 'object-assign'
-import 'array-includes-polyfill'
-import 'es6-set'
-import 'es6-map'
-import 'whatwg-fetch'
+
 
 const app = createApp(App)
 
