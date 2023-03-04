29e7c82405bb48e49e034174c7987d21bf2a36df
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-03-05 00:15:34 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import './assets/normalize.css'
 import './assets/main.css'
 import 'es6-promise/auto'
+import 'object-assign'
+import 'array-includes-polyfill'
+import 'es6-set'
+import 'es6-map'
+import 'whatwg-fetch'
 
 const app = createApp(App)
 
